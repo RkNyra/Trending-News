ca1fc71a622391c4e7bd2f1860dc78cce2818a70
RKNyra <rk.nyra@gmail.com>
2019-10-13 21:48:50 +0300

    write code in the request.py file that creates the first API Call so as to get data from the news API

--- app/requests.py
+++ app/requests.py
@@ -1,4 +1,32 @@
 from app import app
+import urllib.request,json
+from .models import news_article, news_source
+
+Article = news_article.Article
+Source = news_source.Source
 
 #Get the news api key
 api_key = app.config['NEWS_API_KEY']
+
+#Get the news base url
+base_url = app.config["NEWS_API_BASE_URL"]
+
+
+def get_news(updates):
+  '''
+  Function that gets the json response to the url request
+  '''
+  get_news_url = base_url.format(updates,api_key)
+  
+  with urllib.request.urlopen(get_news_url) as url:
+    get_news_data = url.read()
+    get_news_response = json.loads(get_news_data)
+    
+    news_articles = None
+    
+    if get_news_response['articles']:
+      news_articles_list = get_news_response['articles']
+      
+      news_articles = process_articles(news_articles_list)
+
+  return news_articles
