bdc0923b085de8a254f9163413f58551405f5da4
RKNyra <rk.nyra@gmail.com>
2019-10-13 17:34:00 +0300

    create a title for the application's news-article page to give the app a more professional look on navigating to the news artile page

--- app/main/views.py
+++ app/main/views.py
@@ -15,10 +15,11 @@ def index():
   return render_template('index.html', title = title, NewsLink = NewsLink)
 
 
-#Article ID - Navigate to specific news article
+#News-Article Page/ Article ID - Navigate to specific news article
 @app.route('/news/<int:news_id>')
 def news(news_id):
   '''
   View news page function that returns the news article page with the respective news.
   '''
-  return render_template('news.html',id = news_id)
+  title = 'Trending News'
+  return render_template('news.html', title = title, id = news_id)
