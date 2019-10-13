6480328281436741de54e79ded418822b4640791
RKNyra <rk.nyra@gmail.com>
2019-10-13 16:50:43 +0300

    add a news.html file with a dynamic route so that the news article is loaded according to the news id passed in by the app user

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,8 @@ from flask import render_template
 from app import app
 
 #Views
+
+#Homepage
 @app.route('/')
 def index():
   '''
@@ -9,3 +11,12 @@ def index():
   '''
   NewsLink = 'News'
   return render_template('index.html', NewsLink = NewsLink)
+
+
+#Article ID - Navigate to specific news article
+@app.route('/news/<int:news_id>')
+def news(news_id):
+  '''
+  View news page function that returns the news article page with the respective news.
+  '''
+  return render_template('news.html',id = news_id)
