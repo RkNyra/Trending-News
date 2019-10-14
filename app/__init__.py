929e0b6f84d5e99286d939ca1b8394c61dbd03b7
RKNyra <rk.nyra@gmail.com>
2019-10-14 06:18:52 +0300

    install flask_bootstrap and initialize the flask_bootstrap extension in the __init__.py file so that it can be available for use throughout the application

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from config import DevConfig
 
 #Initializing the application
@@ -7,5 +8,8 @@ app = Flask(__name__,instance_relative_config = True)
 #Configuration set-up
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
+
+#Initialize Flask Extensions
+bootstrap = Bootstrap(app)
 
 from .main import views
